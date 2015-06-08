f00948340e510b5e4c4d377242665cd16ce622d9
Feliks Yaretskiy <fyaretskiy@gmail.com>
2015-06-08 19:47:36 -0400

    update

--- functional_test.py
+++ functional_test.py
@@ -2,20 +2,22 @@ import unittest
 
 from selenium import webdriver
 
+
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.implicitly.wait(3)	
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.implicitly.wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
 
-		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
-	
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
+
